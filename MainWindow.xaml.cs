e544fbb4d79f7c77c1676090d858f4f6789aec00
lusama02 <164710899+lusama02@users.noreply.github.com>
2026-05-14 13:33:13 -0300

    primeiro commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace LacoFor;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    {
+        var quantidadeTexto = tbQuantidade.Text;
+        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+
+        if (quantidadeSorteios < 1)
+        {
+            quantidadeSorteios = 1;
+        }
+
+        var sorteador = new Random();
+        
+        for (int contador = 0; contador < quantidadeSorteios; contador++)
+        {
+            tbResultado.Text = sorteador.Next(0, 40000001).ToString();
+            await Task.Delay(1000);
+        }
+    }
+}
