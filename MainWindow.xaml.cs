182685003bc33616a79c84d0341f8c6374bcc339
lusama02 <164710899+lusama02@users.noreply.github.com>
2026-05-14 15:00:25 -0300

    adiciona saldo dinâmico

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,13 +4,18 @@ namespace LacoFor;
 
 public partial class MainWindow : Window
 {
+    public decimal saldoInicial = 1600.00M;
+    
     public MainWindow()
     {
         InitializeComponent();
+        tbSaldo.Text = $"R$ {saldoInicial}";
     }
 
     private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
+        btnSorteio.IsEnabled = false;
+        
         var quantidadeTexto = tbQuantidade.Text;
         var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
 
@@ -23,8 +28,20 @@ public partial class MainWindow : Window
         
         for (int contador = 0; contador < quantidadeSorteios; contador++)
         {
-            tbResultado.Text = sorteador.Next(0, 40000001).ToString();
-            await Task.Delay(1000);
+            if (saldoInicial >= 10)
+            {
+                tbResultado.Text = sorteador.Next(0, 40000001).ToString();
+                saldoInicial -= 10M;
+                tbSaldo.Text = $"R$ {saldoInicial}";
+                await Task.Delay(1000);
+            }
+            else
+            {
+                MessageBox.Show("Você não tem saldo suficiente para realizar o sorteio!");
+                break;
+            }
         }
+
+        btnSorteio.IsEnabled = true;
     }
 }
